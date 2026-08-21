f71b0adbcfdff146b8032bd6d06df0f04f4a9829
zzhpublic <zzhpublic@hotmail.com>
2026-08-21 12:57:06 +0800

    fix: Remove test target (Tests dir doesn't exist)

--- Package.swift
+++ Package.swift
@@ -13,30 +13,25 @@ let package = Package(
         .executable(name: "EmbyVideoPlayerApp", targets: ["EmbyVideoPlayerApp"])
     ],
     dependencies: [
-            // MobileVLCKit for LibVLC - using community SPM distribution
-            .package(url: "https://github.com/MobileVLCKit-SPM/MobileVLCKit-SPM.git", from: "3.6.0"),
+        // MobileVLCKit for LibVLC - using community SPM distribution
+        .package(url: "https://github.com/MobileVLCKit-SPM/MobileVLCKit-SPM.git", from: "3.6.0"),
         // Kingfisher for image loading
         .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.0.0"),
     ],
     targets: [
         .target(
-                    name: "EmbyVideoPlayer",
-                    dependencies: [
-                                .product(name: "MobileVLCKit", package: "MobileVLCKit-SPM"),
-                        .product(name: "Kingfisher", package: "Kingfisher"),
-                    ],
-                    path: "Sources",
-                    resources: []
-                ),
+            name: "EmbyVideoPlayer",
+            dependencies: [
+                .product(name: "MobileVLCKit", package: "MobileVLCKit-SPM"),
+                .product(name: "Kingfisher", package: "Kingfisher"),
+            ],
+            path: "Sources",
+            resources: []
+        ),
         .executableTarget(
             name: "EmbyVideoPlayerApp",
             dependencies: ["EmbyVideoPlayer"],
             path: "App"
-        ),
-        .testTarget(
-            name: "EmbyVideoPlayerTests",
-            dependencies: ["EmbyVideoPlayer"],
-            path: "Tests"
-        ),
+        )
     ]
 )
